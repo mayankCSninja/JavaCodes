727c0f5508080ebc93a7c3409753a95ec901a9e3
Mayank Kumar <mayank.zxc@gmail.com>
2021-03-10 15:31:45 +0530

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,10 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello mayank");
         System.out.println("hello new");
-        System.out.println("hello");
+        System.out.println("jnn");
     }
 }

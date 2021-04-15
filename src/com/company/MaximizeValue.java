c0aae393cf38d6b1b8f17886ddbcbac2e11d9977
Mayank Kumar <mayank.zxc@gmail.com>
2021-04-15 14:45:01 +0530

    commit

--- src/com/company/MaximizeValue.java
+++ src/com/company/MaximizeValue.java
@@ -1,8 +1,8 @@
 package com.company;
 public class MaximizeValue {
     public static void main (String[] args){
-        int[] array = {1,2,3,4,-1,-2,-3};
-        int p = 1, q=1, r=1;
+        int[] array = {-6,3,7,-3,4};
+        int p = -2, q=5, r=4;
 
         int[] sum= new int[array.length-1];
         int max=array[0];
@@ -12,10 +12,10 @@ public class MaximizeValue {
         System.out.print("Prefix_max array = ");
         for (int i=0; i<array.length;i++){
             if (i==0){
-                prefix_max[i]= array[i];
+                prefix_max[i]= p*array[i];
             }
             else {
-                prefix_max[i] = Math.max(prefix_max[i - 1], array[i]);
+                prefix_max[i] = Math.max(prefix_max[i - 1], p*array[i]);
             }
             System.out.print(prefix_max[i] + " ");
         }
@@ -27,10 +27,10 @@ public class MaximizeValue {
         System.out.print("Suffix_max array = ");
         for (int i= array.length-1; i>=0;i--){
             if(i== array.length-1){
-                suffix_max[i] = Math.max(array[i-1],array[i]);
+                suffix_max[i] = Math.max(r*array[i-1],r*array[i]);
             }
             else {
-                suffix_max[i] = Math.max(suffix_max[i+1],array[i]);
+                suffix_max[i] = Math.max(suffix_max[i+1],r*array[i]);
             }
         }
         //printing Suffix max
@@ -41,11 +41,11 @@ public class MaximizeValue {
 
         //for middle term, finding max sum
         for(int mid = 1; mid <= array.length-2;mid++){
-            sum[mid]=p*prefix_max[mid-1]+q*array[mid]+r*suffix_max[mid+1];
+            sum[mid]=prefix_max[mid-1]+q*array[mid]+suffix_max[mid+1];
             if (sum[mid]>max){
                 max=sum[mid];
             }
-            //System.out.println("sum = " +sum[mid]);
+            System.out.println("sum = " +sum[mid]);
 
         }
         System.out.println("Max sum possible = " +max);
